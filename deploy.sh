20ce3603849eae5c43b575a706c91ab56935f4cf
gustavolaino <gustavolaiino@gmail.com>
2024-09-09 10:24:25 -0300

    Atualização do Deploy

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@
 S3_BUCKET_NAME="treinamentoiteris"
 CLOUDFRONT_DISTRIBUTION_ID_FILE="cloudfront_distribution_id.txt"
 INDEX_FILE="index.html"
+INDEX_FILE_PATH="./$INDEX_FILE" # Ajuste o caminho conforme necessário
 
 # 1. Provisionar o bucket S3
 echo "Criando o bucket S3..."
-aws s3api create-bucket --bucket $S3_BUCKET_NAME --region us-east-1
+aws s3api create-bucket --bucket $S3_BUCKET_NAME --region us-east-1 --create-bucket-configuration LocationConstraint=us-east-1
 if [ $? -ne 0 ]; then
-  echo "Erro ao criar o bucket S3."
+  echo "Erro ao criar o bucket S3. O bucket pode já existir."
   exit 1
 fi
 
@@ -17,7 +18,7 @@ echo "Bucket S3 criado com sucesso: $S3_BUCKET_NAME"
 
 # 2. Carregar o conteúdo estático no bucket S3
 echo "Carregando o conteúdo estático no bucket S3..."
-aws s3 cp /home/gustavo/test_aws/$INDEX_FILE s3://$S3_BUCKET_NAME/
+aws s3 cp $INDEX_FILE_PATH s3://$S3_BUCKET_NAME/
 if [ $? -ne 0 ]; then
   echo "Erro ao carregar o conteúdo no bucket S3."
   exit 1
@@ -39,5 +40,3 @@ fi
 
 echo "Distribuição CloudFront criada com sucesso. ID: $DISTRIBUTION_ID"
 echo $DISTRIBUTION_ID > $CLOUDFRONT_DISTRIBUTION_ID_FILE
-
-echo "Configuração concluída."
